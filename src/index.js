af189c3d003b308b31098cc1c089218eaf27b438
Aljosha Cedric <acmerz@posteo.de>
2020-04-14 16:41:42 +0200

    publish Easy Sprite

---
+++ src/index.js
@@ -0,0 +1,185 @@
+const fs=require('fs-extra');
+const PATH=require('path');
+const SVGO=require('svgo');
+const {parse, stringify} = require('svgson');
+
+module.exports={
+	add,
+	getIcons,
+	removeIcon
+};
+
+/**
+ * Adds the given SVG(s) (either files or codes) to a provided sprite sheet. If the sprite sheet does not exist, it will be created. The path to the sprite sheet will be returned
+ * @param {String} sprite - path to the sprite sheet
+ * @param {String} svgSource - the source of the SVG as a path string. This can either point to a single .svg file or a directory of SVG files
+ * @param {Boolean} isCode - a boolean which has to be provided when the SVG source is raw SVG code
+ * @returns {Promise<String>}
+ */
+function add(sprite,svgSource,isCode){
+	return new Promise(async (resolve, reject) =>{
+		try{
+			let spriteObj={
+				name:'svg',
+				type:'element',
+				value:'',
+				attributes:{xmlns: 'http://www.w3.org/2000/svg'}
+			},report={msg:`The following IDs already exist in ${sprite}`,ids:[]};
+			if(await fs.exists(sprite))
+				spriteObj=await parse(fs.readFileSync(sprite,'utf8'));
+			else
+				spriteObj.children=[{
+					name: 'defs',
+					type: 'element',
+					value: '',
+					attributes: {},
+					children:[]
+				}];
+			const existingIDs=await getIcons(sprite);
+			const symbols=[];
+			let files;
+			if(PATH.extname(svgSource) && PATH.extname(svgSource)==='.svg' || isCode)
+				files=[svgSource];
+			else if(!PATH.extname(svgSource))
+				files=fs.readdirSync(svgSource, 'utf8').filter(file=>{return PATH.extname(file)==='.svg'}).map(file=>{return PATH.join(svgSource,file)});
+			for(const file of files){
+				const {symbol,definitions,duplicates}=await makeSVG(existingIDs,file,isCode);
+				if(duplicates.length)
+					report.ids.push(...duplicates);
+				else{
+					spriteObj.children[0].children.push(definitions);
+					symbols.push(symbol);
+				}
+			}
+			spriteObj.children.push(symbols);
+			fs.outputFileSync(sprite,stringify(spriteObj));
+			if(report.ids.length)
+				return resolve(`File saved as ${sprite}. ${report.msg}: ${report.ids.join()}`);
+			else
+				return resolve(`File saved as ${sprite}.`);
+		}catch (e) {return reject(new Error(e))}
+	});
+}
+
+/**
+ * Generates an object representation of the SVG sprite sheet and returns it. {@see objectifySvg}
+ * @param {Array<String>} ids - an array of existing IDs in the sprite {@see getIcons}
+ * @param {String} file - the path to an SVG source
+ * @param {Boolean} isCode - a boolean indicating whether the SVG source is raw svg code or contained in a file
+ * @returns {Promise<Object>}
+ */
+function makeSVG(ids,file,isCode) {
+	return new Promise(async(resolve, reject) =>{
+		try{
+			const duplicates=[];
+			let symbol={},definitions={};
+			const svgObj=await objectifySvg(file,isCode,true);
+			const id=nameToID(PATH.basename(file, '.svg'));
+			if(ids.includes(id))
+				duplicates.push(id);
+			else{
+				const idx=svgObj.children.indexOf(svgObj.children.filter(child=>{return child.name==='defs'})[0]);
+				definitions=svgObj.children.splice(idx, 1)[0].children.map(child=>{
+					child.attributes.id=`${id}-${child.attributes.id}`;
+					return child;
+				});
+				symbol={
+					name:'symbol',
+					type:'element',
+					value:'',
+					attributes:{id,viewBox:svgObj.attributes.viewBox},
+					children:svgObj.children.map(child=>{
+						if(child.attributes.hasOwnProperty('filter'))
+							child.attributes.filter=`url(#${id}-${child.attributes.filter.match(/filter.+?(?=\))/)[0]})`;
+						return child
+					})
+				};
+			}
+			return resolve({duplicates,symbol,definitions});
+		}catch (e) {return reject(new Error(e))}
+	});
+}
+
+/**
+ * Parses an SVG string (either from file or from raw SVG code) and returns the SVG as an object to {@see makeSVG}. Modulee used for SVG parsing: {@link https://www.npmjs.com/package/svgson}
+ * @param {String} file - the path to the SVG file or raw SVG code
+ * @param {Boolean} isCode - indicates whether the file is raw SVG code or not
+ * @param {Boolean} optimize - whether to optimize the SVG data. Needed because symbol elements are stripped when parsing whole sprite sheet.
+ * @returns {Promise<Object>}
+ */
+function objectifySvg(file,isCode,optimize) {
+	return new Promise(async(resolve, reject) =>{
+		try{
+			const svgo=new SVGO(require('./SVGO.config').options);
+			const data=isCode ? file : await fs.readFile(file,'utf8');
+			if(optimize){
+				const svg=await svgo.optimize(data);
+				return resolve(await parse(svg.data));
+			}
+			else
+				return resolve(await parse(data));
+		}catch (e) {return reject(new Error(e))}
+	});
+}
+
+/**
+ * Used to generate symbol IDs. IDs are derived from the lowercase filenames, which are hyphenated. E.g: Add User becomes add-user
+ * @param {String} name
+ * @returns {string}
+ */
+function nameToID(name) {
+	const arr=name.toLowerCase().split(' ');
+	return arr.length===1 ? arr[0] : arr.join('-');
+}
+
+/**
+ * Extracts all symbols from the SVG object {@see objectifySvg} and returns a map of their id attribute
+ * @param {String} sprite - path to the sprite sheet
+ * @returns {Promise<Array<String>>}
+ */
+function getIcons(sprite){
+	return new Promise(async (resolve, reject) =>{
+		try {
+			if(await fs.exists(sprite)){
+				const spriteData=await objectifySvg(sprite,false,false);
+				return resolve(spriteData.children
+					.filter(d => {return d.name === 'symbol'})
+					.map(d => {return d.attributes.id}));
+			}
+			else
+				return resolve([]);
+		}catch (e) {return reject(new Error(e))}
+	});
+}
+
+/**
+ * Deletes a specific ID from the given sprite sheet
+ * @param {String} sprite - the path to the sprite file
+ * @param {String} id - the id to be removed
+ * @returns {Promise<String>}
+ */
+function removeIcon(sprite,id) {
+	return new Promise(async (resolve, reject) =>{
+		try {
+			const found=[];
+			let spriteObj=await objectifySvg(sprite,false,false);
+			spriteObj.children.forEach(child=>{
+				if(child.attributes.id===id)
+					found.push(child);
+				else if(child.name==='defs'){
+					found.push(child.children.filter(child => {
+						return child.attributes.id.match(new RegExp(`${id}-filter.+`))
+					})[0]);
+				}
+			});
+			found.forEach(f=>{
+				if(f.name==='symbol')
+					spriteObj.children.splice(spriteObj.children.indexOf(f),1);
+				else
+					spriteObj.children[0].children.splice(spriteObj.children[0].children.indexOf(found[1]),1);
+			});
+			fs.outputFileSync(sprite,stringify(spriteObj));
+			return resolve(`Icon "${id}" removed from ${sprite}`)
+		}catch (e) {return reject(new Error(e))}
+	});
+}
---
+++ test/app.js
@@ -0,0 +1,24 @@
+const PATH=require('path');
+const svgSprite=require('../src/index');
+const sprite=PATH.join(__dirname,'icons.svg');
+
+async function test() {
+	try {
+		console.log('---------------------\n');
+		console.log('Starting test');
+		console.log('---------------------\n');
+		console.log(await svgSprite.add(sprite, PATH.join(__dirname,'test icons'),false));
+		console.log('---------------------\n');
+		console.log(await svgSprite.getIcons(sprite));
+		console.log('---------------------\n');
+		console.log(await svgSprite.add(sprite, PATH.join(__dirname,'more icons','add.svg'),false));
+		console.log('---------------------\n');
+		console.log(await svgSprite.removeIcon(sprite, 'user'));
+		console.log('---------------------\n');
+		console.log(await svgSprite.getIcons(sprite));
+		console.log('---------------------\n');
+		console.log('Test finished');
+	}catch (e) {throw e}
+}
+
+test();
